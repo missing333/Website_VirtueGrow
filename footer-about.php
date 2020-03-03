4e8e2df35f3bb959a2ff34fc88122e57f05f75ba
Marc Missildine <mmissildine@nqueue.com>
2020-03-03 14:30:17 -0700

    updated page titles

--- footer-about.php
+++ footer-about.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>VirtueGrow</title>
+  <title>About</title>
 	<link rel="shortcut icon" href="Art\Icons\LogoFavicon32.png" type="image/x-icon">
 
   <link href='https://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
