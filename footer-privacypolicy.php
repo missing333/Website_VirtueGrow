7c147b80c30ed55300e3aa8d6333fa2bd0135ebf
Marc Missildine <mmissildine@nqueue.com>
2020-03-03 14:10:02 -0700

    added google analytics

--- footer-privacypolicy.php
+++ footer-privacypolicy.php
@@ -14,6 +14,16 @@
 
   <script type="text/javascript" src="jscript.js"></script>
   <link rel="stylesheet" href="style.css">
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-112386024-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-112386024-1');
+</script>
 </head>
 
 
