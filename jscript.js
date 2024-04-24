1ca37edc638fdb11696b38bd98d741d2b9daddcc
Marc <mmissing3@gmail.com>
2024-04-24 10:51:03 -0700

    update analytics

--- jscript.js
+++ jscript.js
@@ -4,17 +4,11 @@ function openVirtue(virtueName) {
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
+
   document.getElementById(virtueName).style.display = "grid";
-  var elem = $('#scrollHere');
-  if(elem) {
-    $('html').scrollTop(elem.offset().top);
-    $('html').scrollLeft(elem.offset().left);
+  var elem = $("#scrollHere");
+  if (elem) {
+    $("html").scrollTop(elem.offset().top);
+    $("html").scrollLeft(elem.offset().left);
+  }
 }
-}
-
-
-
-
-
-$("#header").load("header.html");
-$("#footer").load("footer.html");
